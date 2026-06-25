2d3ba1cd99cb5e361194c50be1a84395def6e6c8
Katlyn Lorimer <katlyn@katlyn.dev>
2026-06-24 17:14:08 -0800

    images: allow png and jpg images

--- Dockerfile
+++ Dockerfile
@@ -1,4 +1,4 @@
-FROM node:20-alpine as base
+FROM node:24-alpine AS base
 WORKDIR /usr/src/userpfp-bot
 ENV DOCKER="TRUE"
 ENV PNPM_HOME="/pnpm"
--- src/utils/images.ts
+++ src/utils/images.ts
@@ -4,34 +4,37 @@ import env from "../config/env.js"
 import octokit from "../config/octokit.js"
 
 export interface ValidatedImage {
-  type: string
-  ext: string
-  url: string
-  stillUrl: string
+  type: string;
+  ext: string;
+  url: string;
+  stillUrl: string;
 }
 export interface ImageData {
-  type: string
-  ext: string
-  animated: Buffer
-  still: Buffer
+  type: string;
+  ext: string;
+  animated: Buffer;
+  still: Buffer;
 }
 
 const aspectRatioVariance = 0.1 // Allow 10% different in size between width and height
-const allowedContentTypes = ["image/gif"]
+const allowedContentTypes = ["image/gif", "image/png", "image/jpeg"]
 const maxImageBytes = 12e6 // 12MB
 
 // Error class used when issues regarding image processing occur
 export class ImageError extends Error {}
 
-export function getStillUrl (image: AttachmentData|EmbedImage) {
+export function getStillUrl(image: AttachmentData | EmbedImage) {
   const stillUrl = new URL(image.proxy_url!)
   stillUrl.searchParams.set("format", "png")
   return stillUrl.toString()
 }
 
-export function validateImage (attachment: AttachmentData): ValidatedImage {
-  if (attachment.content_type === undefined || !allowedContentTypes.includes(attachment.content_type)) {
-    throw new ImageError("Image must be a gif.")
+export function validateImage(attachment: AttachmentData): ValidatedImage {
+  if (
+    attachment.content_type === undefined ||
+    !allowedContentTypes.includes(attachment.content_type)
+  ) {
+    throw new ImageError("Attachment must be an image.")
   }
 
   // Make sure the image is square-ish
@@ -42,14 +45,17 @@ export function validateImage (attachment: AttachmentData): ValidatedImage {
     attachment.width / attachment.height >= 1 + aspectRatioVariance ||
     attachment.height / attachment.width >= 1 + aspectRatioVariance
   ) {
-    throw new ImageError("The provided image must be a square (or close to it).")
+    throw new ImageError(
+      "The provided image must be a square (or close to it).",
+    )
   }
 
   // Check image file size
   if (attachment.size > maxImageBytes) {
-    throw new ImageError("The provided image is too large and must be compressed.")
+    throw new ImageError(
+      "The provided image is too large and must be compressed.",
+    )
   }
-
 
   return {
     type: attachment.content_type,
@@ -59,7 +65,9 @@ export function validateImage (attachment: AttachmentData): ValidatedImage {
   }
 }
 
-export async function validateAndDownloadImage (image: ValidatedImage): Promise<ImageData> {
+export async function validateAndDownloadImage(
+  image: ValidatedImage,
+): Promise<ImageData> {
   const imageRequests = await Promise.all([
     fetch(image.url, {
       headers: {
@@ -72,13 +80,14 @@ export async function validateAndDownloadImage (image: ValidatedImage): Promise<
       }
     })
   ])
-
   if (imageRequests.some(v => !v.ok)) {
     throw new ImageError("Unable to download image!")
   }
 
-  const imageArrayBuffers = await Promise.all(imageRequests.map(i => i.arrayBuffer()))
-  const [ animated, still ] = imageArrayBuffers.map(ab => Buffer.from(ab))
+  const imageArrayBuffers = await Promise.all(
+    imageRequests.map(i => i.arrayBuffer()),
+  )
+  const [animated, still] = imageArrayBuffers.map(ab => Buffer.from(ab))
 
   return {
     type: image.type!,
@@ -88,7 +97,11 @@ export async function validateAndDownloadImage (image: ValidatedImage): Promise<
   }
 }
 
-async function uploadImageFile (message: string, imagePath: string, buff: Buffer) {
+async function uploadImageFile(
+  message: string,
+  imagePath: string,
+  buff: Buffer,
+) {
   let sha: string | undefined = undefined
   try {
     const existingFileData = await octokit.repos.getContent({
@@ -116,29 +129,36 @@ async function uploadImageFile (message: string, imagePath: string, buff: Buffer
   })
 }
 
-export async function updateUserAvatarImage (userId: string, image: ImageData) {
+export async function updateUserAvatarImage(userId: string, image: ImageData) {
   const imageBasePath = `Avatars/${userId[0]}/${userId}`
 
-  await uploadImageFile(`Updating animated avatar for ${userId}`, `${imageBasePath}.gif`, image.animated)
-  await uploadImageFile(`Updating still avatar for ${userId}`, `${imageBasePath}.png`, image.still)
+  await uploadImageFile(
+    `Updating animated avatar for ${userId}`,
+    `${imageBasePath}.gif`,
+    image.animated,
+  )
+  await uploadImageFile(
+    `Updating still avatar for ${userId}`,
+    `${imageBasePath}.png`,
+    image.still,
+  )
 
   return `https://raw.githubusercontent.com/${env.repos.image.owner}/${env.repos.image.name}/main/${imageBasePath}`
 }
 
-export async function deleteUserAvatarImage (userId: string) {
+export async function deleteUserAvatarImage(userId: string) {
   // Get a list of all avatar files in the directory
   const directoryListing = await octokit.repos.getContent({
     owner: env.repos.image.owner,
     repo: env.repos.image.name,
     path: `/Avatars/${userId[0]}`
   })
-
   if (!Array.isArray(directoryListing.data)) {
     throw new Error("User directory not correct type")
   }
 
-  const matchedAvatars = directoryListing.data.filter(v => v.name.startsWith(`${userId}.`))
-
+  const matchedAvatars = directoryListing.data.filter(v =>
+    v.name.startsWith(`${userId}.`))
   for (const avatar of matchedAvatars) {
     const response = await octokit.repos.deleteFile({
       owner: env.repos.image.owner,
@@ -148,7 +168,9 @@ export async function deleteUserAvatarImage (userId: string) {
       sha: avatar.sha
     })
     if (response.data.commit.sha === undefined) {
-      throw new Error("Unexpected response from Github when deleting user images")
+      throw new Error(
+        "Unexpected response from Github when deleting user images",
+      )
     }
   }
 }
